73eca412ff654095d20eeafb6a4680950ceff6f7
Elián Fabián <elian.fabian.el.belaizi@gmail.com>
2024-01-12 21:04:02 +0100

    Simplified Get-WcmItem and changed error message
    We no longer support returning references, just a copy.

--- PSWindowsContextMenuManager/Public/Functions/Get-WcmItem.ps1
+++ PSWindowsContextMenuManager/Public/Functions/Get-WcmItem.ps1
@@ -8,17 +8,63 @@ function Get-WcmItem
 
         [Parameter(Mandatory=$true)]
         [ValidateSet('File', 'Directory', 'Desktop', 'Drive')]
-        [string] $Type,
-
-        [switch] $Copy
+        [string] $Type
     )
 
-    if ($Copy)
+    $registryPath = Resolve-PathKey -LiteralPathKey $LiteralPathKey -Type $Type
+
+    [Microsoft.Win32.RegistryKey]`
+    $registryItem = Get-Item -LiteralPath $registryPath -ErrorAction Ignore
+
+    if (-not $registryItem)
     {
-        return Get-WcmItemCopy -LiteralPathKey $LiteralPathKey -Type $Type
+        Write-Error "Cannot find context menu item with key '$LiteralPathKey' and type '$Type' because it does not exist. Full path: '$registryPath'."
+        return
+    }
+
+    $childName = $registryItem.GetSubKeyNames()[0]
+
+    $isGroupItem = ('Subcommands' -in $registryItem.Property) -and ($childName -eq 'Shell')
+    $isCommandItem = ($childName -eq 'Command') -and (-not $isGroupItem)
+
+    $itemType = if ($isGroupItem)
+    {
+        'Group'
+    }
+    elseif ($isCommandItem)
+    {
+        'Command'
     }
     else
     {
-        return Get-WcmItemReference -LiteralPathKey $LiteralPathKey -Type $Type
+        Write-Error "The item '$registryPath' is not a Windows context menu item."
+        return $null
     }
+
+    $wcmItem = [PSCustomObject] @{
+        Key          = $registryItem.PSChildName
+        RegistryPath = $registryItem.PSPath
+        Type         = $Type
+        IconPath     = $registryItem.GetValue('Icon')
+        Position     = $registryItem.GetValue('Position')
+        IsExtended   = $null -ne $registryItem.GetValue('Extended')
+    }
+
+    switch ($itemType)
+    {
+        Command
+        {
+            $wcmItem
+                | Add-Member -Name Name    -Value $registryItem.GetValue('')                                   -MemberType NoteProperty -PassThru
+                | Add-Member -Name Command -Value (Get-Item -LiteralPath "$registryPath\Command").GetValue('') -MemberType NoteProperty
+        }
+        Group
+        {
+            $wcmItem
+                | Add-Member -Name Name       -Value $registryItem.GetValue('MUIVerb')                         -MemberType NoteProperty -PassThru
+                | Add-Member -Name ChildCount -Value (Get-Item -LiteralPath "$registryPath\Shell").SubKeyCount -MemberType NoteProperty
+        }
+    }
+
+    return $wcmItem
 }
